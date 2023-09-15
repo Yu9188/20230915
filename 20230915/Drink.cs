ba833108a3fd232d0d273aa8b1dcd1c49b263143
Yuuu <Yuuu@DESKTOP-096DUOQ>
2023-09-15 13:16:27 +0800

    加入專案檔案。

---
+++ 20230915/Drink.cs
@@ -0,0 +1,9 @@
+﻿namespace _20230915
+{
+    internal class Drink
+    {
+        public String Name { get; set; }
+        public String Size { get; set; }
+        public int Price { get; set; }
+    }
+}
